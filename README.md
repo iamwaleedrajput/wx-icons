3516cea0ef1241e07ca7a8b325b3d6264d88886a
iamwaleedrajput <waleedrajput7453@gmail.com>
2022-01-27 14:20:21 +0500

    new version

--- README.md
+++ README.md
@@ -3,3 +3,46 @@
 ```
 npm install @wxui/icons
 ```
+```js
+
+for Music
+
+import React from 'react';
+import WxMusic from 'wx-icons';
+
+function App() {
+  return <WxMusic color='' size={40}/>;
+}
+
+export default App;
+
+```
+
+```js
+
+for Twitch
+
+import React from 'react';
+import WxTwitch from 'wx-icons';
+
+function App() {
+  return <WxTwitch color='' size ={40}/>;
+}
+
+export default App;
+
+```
+```js
+
+for Windows
+
+import React from 'react';
+import WxWindows from 'wx-icons';
+
+function App() {
+  return <WxWindows color='#00A4EF' size={40}/>;
+}
+
+export default App;
+
+```
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Facebook from "./Facebook";
 import Google from "./Google";
 import Menu from "./Menu";
@@ -7,3 +8,29 @@ import Apple from "./Apple";
 import Github from "./Github";
 
 export { Facebook, Twitter, Instagram, Google, Menu, Apple, Github };
+=======
+import WxFacebook from "./WxFacebook";
+import WxGoogle from "./WxGoogle";
+import WxMenu from "./WxMenu";
+import WxTwitter from "./WxTwitter";
+import WxInstagram from "./WxInstagram";
+import WxApple from "./WxApple";
+import WxGithub from "./WxGithub";
+import WxMusic from './WxMusic'
+import WxTwitch from "./WxTwitch";
+import WxWindows from "./WxWindows";
+
+
+export {
+  WxFacebook,
+  WxTwitter,
+  WxInstagram,
+  WxGoogle,
+  WxMenu,
+  WxApple,
+  WxGithub,
+  WxMusic,
+  WxTwitch,
+  WxWindows
+};
+>>>>>>> d5a4695f75cd05c32318cd2c9e87f3f95c40a142
