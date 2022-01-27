822bbe0c2ada93e3fb85176bb0cfa416dcba8a82
iamwaleedrajput <waleedrajput7453@gmail.com>
2022-01-27 14:20:57 +0500

    new versions

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Facebook from "./Facebook";
 import Google from "./Google";
 import Menu from "./Menu";
@@ -6,31 +5,19 @@ import Twitter from "./Twitter";
 import Instagram from "./Instagram";
 import Apple from "./Apple";
 import Github from "./Github";
-
-export { Facebook, Twitter, Instagram, Google, Menu, Apple, Github };
-=======
-import WxFacebook from "./WxFacebook";
-import WxGoogle from "./WxGoogle";
-import WxMenu from "./WxMenu";
-import WxTwitter from "./WxTwitter";
-import WxInstagram from "./WxInstagram";
-import WxApple from "./WxApple";
-import WxGithub from "./WxGithub";
-import WxMusic from './WxMusic'
-import WxTwitch from "./WxTwitch";
-import WxWindows from "./WxWindows";
-
+import Music from "./Music";
+import Twitch from "./Twitch";
+import Windows from "./Windows";
 
 export {
-  WxFacebook,
-  WxTwitter,
-  WxInstagram,
-  WxGoogle,
-  WxMenu,
-  WxApple,
-  WxGithub,
-  WxMusic,
-  WxTwitch,
-  WxWindows
+  Facebook,
+  Twitter,
+  Instagram,
+  Google,
+  Menu,
+  Apple,
+  Github,
+  Music,
+  Twitch,
+  Windows,
 };
->>>>>>> d5a4695f75cd05c32318cd2c9e87f3f95c40a142
